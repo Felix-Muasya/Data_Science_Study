106aff8edecf9a8a0e76ed01a44baa606b41fd52
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2023-01-30 15:44:39 +0300

    Create huffman_code_example.py

---
+++ Information_Theory/Huffman Encoding/huffman_code_example.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from heapq import heappush, heappop
+
+def huffman_encode(text):
+    frequency = Counter(text)
+    heap = [[weight, [char, ""]] for char, weight in frequency.items()]
+    heap = [[weight, [char, "0"]] for char, weight in frequency.items()]
+    heap = [[weight, [char, "1"]] for char, weight in frequency.items()]
+    heappush(heap, [weight1 + weight2, [char1 + char2, code1 + code2]])
+    return dict(heappop(heap))
+
+text = "ADBADEDBBDD"
+encoded_text = "".join(huffman_encode(text)[c] for c in text)
+print("Encoded text:", encoded_text)
